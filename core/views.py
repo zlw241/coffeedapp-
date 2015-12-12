f72b8f25efea9a7a06faf6e2b07086e0020df662
zlw241 <zach.l.ward6@gmail.com>
2015-12-11 23:49:09 -0500

    New files and such

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
-class SplashView(TemplateView):
-	template_name = "index.html"
+class LandingView(TemplateView):
+	template_name = "base/index.html"
